5046270e4926c400ee31eaba44a45ec97b362f3d
kena0ki <ken3574.25@gmail.com>
2022-12-19 17:39:04 +0900

    polish text

--- pages/1st.tsx
+++ pages/1st.tsx
@@ -5,10 +5,10 @@ import Button from '../components/buttons';
 
 export default function Home() {
 
-  const title=`-  AI Font Demo  -`;
+  const title=`AI Font Gallary`;
   const description=
-`AIにより自動生成されたフォントを眺めて楽しむサイトです。
-生成で参照するフォントは数枚程度です。`;
+`AIによるフォント自動生成を試すことができるサイトです。
+生成に必要となるフォント画像は数枚です。`;
   const disclaimer1= `現状では`;
   const disclaimer2= `FTransGAN`;
   const disclaimer3= `というAIのモデルにより生成を行います。`;
@@ -19,10 +19,14 @@ export default function Home() {
       <p className={styles.description}>{description}</p>
       <div className={styles.button}>
         <Link href="/2nd">
-          <Button size="big" >Try it!</Button>
+          <Button size="big" >Try!</Button>
         </Link>
       </div>
-      <p className={styles.disclaimer}>{disclaimer1}<a>{disclaimer2}</a>{disclaimer3}</p>
+      <p className={styles.disclaimer}>
+        {disclaimer1}
+        <a target="_blank" rel="noreferrer" href="https://openaccess.thecvf.com/content/WACV2021/papers/Li_Few-Shot_Font_Style_Transfer_Between_Different_Languages_WACV_2021_paper.pdf" >{disclaimer2}</a>
+        {disclaimer3}
+      </p>
     </Layout>
   );
 }
