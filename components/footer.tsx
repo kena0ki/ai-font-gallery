c58ea2a1bb5cc5896c47b9640fad9d1233e94898
kena0ki <ken3574.25@gmail.com>
2022-12-20 01:24:15 +0900

    add link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,16 @@
+import Button from './buttons';
 import styles from './footer.module.scss';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.leftMargin}></div>
-      <div className={styles.signature}>Made by kena0ki</div>
+      <div className={styles.signature}>
+        Made by
+        <a target="_blank" rel="noreferrer" href="https://github.com/kena0ki">
+          <Button uitype="ghost">kena0ki</Button>
+        </a>
+      </div>
     </footer>
   );
 }
