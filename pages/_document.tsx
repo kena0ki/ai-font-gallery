7a0bb76c2c66bdcafc3116bcb3488cd44caa7bc7
kena0ki <ken3574.25@gmail.com>
2022-12-21 19:20:11 +0900

    og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="og:image" content="https://raw.githubusercontent.com/kena0ki/ai-font-gallery/main/public/logo_og.png" />
-        <meta name="og:image:alt" content="Logo" />
-        <meta name="og:title" content={SITE_NAME} />
-        <meta name="og:description" content="Try few-shot font generation models" />
         <meta name="twitter:card" content="summary" />
+        <meta property="og:image" content="https://raw.githubusercontent.com/kena0ki/ai-font-gallery/main/public/logo_og.png" />
+        <meta property="og:image:alt" content="Logo" />
+        <meta property="og:title" content={SITE_NAME} />
+        <meta property="og:description" content="Try few-shot font generation models" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Yuji+Boku&display=swap" rel="stylesheet" />
