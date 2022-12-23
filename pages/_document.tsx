51f9e59b680426857aac59a6fd482746aa543760
kena0ki <ken3574.25@gmail.com>
2022-12-23 22:57:31 +0900

    seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { SITE_NAME } from '../utils/constants';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/*<!-- Google tag (gtag.js) -->*/}
+        <Script
+          id="gtag"
+          src="https://www.googletagmanager.com/gtag/js?id=G-QMV6SXSSSD"
+          strategy="afterInteractive">
+        </Script>
+        <Script id="datalayer" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-QMV6SXSSSD');
+          `}
+        </Script>
         <meta name="twitter:card" content="summary" />
         <meta property="og:image" content="https://raw.githubusercontent.com/kena0ki/ai-font-gallery/main/public/logo_og.png" />
         <meta property="og:image:alt" content="Logo" />
