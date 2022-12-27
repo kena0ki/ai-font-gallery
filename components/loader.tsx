c93e05c516ec9a163a73213e9344bad459012c57
kena0ki <ken3574.25@gmail.com>
2022-12-27 23:19:39 +0900

    width height

--- components/loader.tsx
+++ components/loader.tsx
@@ -8,7 +8,7 @@ export default function Loader(props: Props) {
     <div className={`${styles.loaderBackground} ${!props.loading&&styles.off}`}>
       <div className={`${styles.loaderContainer} ${props.className}`}>
         <div>
-          <svg x="0px" y="0px" width={props.width} height={props.height} viewBox="0 0 50 50" >
+          <svg x="0px" y="0px" style={{width:props.width, height:props.height}} viewBox="0 0 50 50" >
           <path d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
             <animateTransform attributeType="xml"
               attributeName="transform"
